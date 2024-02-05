9526220037e1c4a1435255bb848b7463d286d57a
jjeunv <jjeunv@gmail.com>
2024-02-06 01:30:48 +0900

    [ADD] 2422 python solution

---
+++ BruteForce/2422_한윤정이이탈리아에가서아이스크림을사먹는데.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+icecream = [[True for _ in range(n + 1)] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    icecream[a][b] = False
+    icecream[b][a] = False
+
+ans = 0
+
+for i in range(1, n - 1):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n + 1):
+            if icecream[i][j] and icecream[j][k] and icecream[k][i]:
+                ans += 1
+print(ans)
+1234
+12
+134
+234
