18539277c3d7c1efcfcbcb9d089efc4c1072aac3
jjeunv <jjeunv@gmail.com>
2024-02-06 01:31:41 +0900

    [ADD] 16439 python solution

---
+++ BruteForce/16439_치킨치킨치킨.py
@@ -0,0 +1,19 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+preference = [list(map(int, input().split())) for _ in range(n)]
+
+chicken = [i for i in range(1, m + 1)]
+
+ans = 0
+for f, s, t in combinations(chicken, 3):
+    sum = 0
+    for p in preference
+        sum += max(p[f - 1], p[s - 1], p[t - 1])
+    ans = max(ans, sum)
+
+print(ans)
