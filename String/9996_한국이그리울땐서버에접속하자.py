c4cbb273d29404b5d812df8617477982c4498a89
jjeunv <jjeunv@gmail.com>
2024-03-01 23:42:51 +0900

    [MODIFY] Remove unnecessary code in 9996

--- String/9996_한국이그리울땐서버에접속하자.py
+++ String/9996_한국이그리울땐서버에접속하자.py
@@ -3,14 +3,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-pattern = input().strip().split("*")
-file = [input().strip() for _ in range(n)]
+pattern = list(input().strip())
+file = [list(input().strip()) for _ in range(n)]
 
-front = pattern[0]
-back = pattern[1]
-print(front, back)
+idx = pattern.index("*")
+front = pattern[:idx]
+back = pattern[:idx:-1]
+
 for i in file:
-    print(i[: len(front)], i[len(i) - 1 : len(i) - 1 - len(back) : -1])
     if len(i) < len(front) or len(i) < len(back) or len(i) < len(pattern) - 1:
         print("NE")
         continue
